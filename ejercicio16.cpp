8917ccaee39204203503c62fd281d7f60bc48b9a
Elvis-199-afk <earocutipaq@unjbg.edu.pe>
2024-07-03 00:13:03 -0500

    Agregando el dieciseisavo ejercicio, que trata de ingresar una cadena, que obiando todos los simbolos excepto los numeros, sume dos a este numero

--- ejercicio16.cpp
+++ ejercicio16.cpp
@@ -1,5 +1,56 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
+
+bool esnumero(char);
+int numero(char);
+
+int tama(char *cadena){
+	int tam=-1;
+	for(int i=0; cadena[i]!='\0'; i++){
+		tam++;	
+	}
+	return tam;
+}
+
+void descartar_sim(char *cadena){
+	bool ver;
+	int j;
+	int tam=tama(cadena);
+	for(int i=0; i<=tam; i++){
+		ver=esnumero(cadena[i]);
+		if(!ver){
+			j=i;
+			while(cadena[j]!='\0'){
+				cadena[j]=cadena[j+1];
+				j++;
+			}
+			tam--;
+			i--;		
+		}
+	}
+}
+
+void convertir(char cadena[]){
+	int num=0, num_c;
+	int tam=tama(cadena);
+	for(int i=0; i<=tam; i++){
+		num_c=numero(cadena[i]);
+		num+=num_c*(pow(10, tam-i));
+	}
+	cout<<num<<" -> "<<num+2;
+}
+
+
+int main(){
+	char cadena[30];
+	cout<<"Ingrese la cadena: "<<endl;
+	cin.getline(cadena, 30);
+	cout<<endl;
+	descartar_sim(cadena);
+	convertir(cadena);
+	return 0;
+}
 
 bool esnumero(char num){
 	bool verdad=false;
@@ -50,27 +101,51 @@ bool esnumero(char num){
 	}
 }
 
-void descartar_sim(char *cadena){
-	bool ver;
-	int j;
-	for(int i=0; cadena[i]!='\0'; i++){
-		ver=esnumero(cadena[i]);
-		if(!ver){
-			j=i;
-			while(cadena[j]!='\0'){
-				cadena[j]=cadena[j+1];
-				j++;
-			}
-			cadena[j]='\0';			
-		}
+int numero(char num){
+	int n;
+	switch(num){
+		case '0':
+			n=0;
+			return n;
+			break;
+		case '1':
+			n=1;
+			return n;
+			break;
+		case '2':
+			n=2;
+			return n;
+			break;
+		case '3':
+			n=3;
+			return n;
+			break;
+		case '4':
+			n=4;
+			return n;
+			break;
+		case '5':
+			n=5;
+			return n;
+			break;
+		case '6':
+			n=6;
+			return n;
+			break;
+		case '7':
+			n=7;
+			return n;
+			break;
+		case '8':
+			n=8;
+			return n;
+			break;
+		case '9':	
+			n=9;
+			return n;
+			break;
+		default:
+			return n;
+			break;
 	}
-}
-
-
-int main(){
-	char cadena[30];
-	cin.getline(cadena, 30);
-	descartar_sim(cadena);
-	cout<<cadena;
-	return 0;
 }
