724b76813725ca84d7ae67cd88d39a0652a384b2
Elvis-199-afk <earocutipaq@unjbg.edu.pe>
2024-07-03 00:20:04 -0500

    Agregando el dieciochoavo ejercicio, que trata de comprobar si el balance de parentesis es correcto o no, de una cadena y parentesis

---
+++ ejercicio18.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+void parentesis(char *c){
+	int i=0, pa=0, pc=0;
+	bool op=true;
+	while(c[i]!='\0'){
+		if(c[i]=='('){
+			pa++;
+			i++;
+		}else if(c[i]==')'){
+			if(pa>pc){
+				pc++;
+			}else{
+				cout<<"esta mal";
+				break;
+			}
+		}else{
+			i++;
+		}
+	}
+	if(c[i]=='\0'){
+		if(pa==pc){
+			cout<<"esta bien";
+		}else{
+			cout<<"esta mal";
+		}
+	}
+}
+
+int main(){
+	char c[50];
+	cin.getline(c, 50);
+	parentesis(c);
+	return 0;
+}
