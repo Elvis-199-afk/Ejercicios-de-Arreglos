0fc880e0ed4ea0e40ee923e73fd9f914382053bb
Elvis-199-afk <earocutipaq@unjbg.edu.pe>
2024-07-04 08:27:49 -0500

    Update ejercicio15.cpp

--- ejercicio15.cpp
+++ ejercicio15.cpp
@@ -11,42 +11,9 @@ int str_cmp(char *str1,char *str2){
 		}else if(str1[i]>str2[i]){
 			cmp=1;
 			break;
-		}
-	}
-	return cmp;
-}
-
-int main(){
-	int num;
-	char str1[30],str2[30];
-	cout<<"Ingrese la primera cadena: ";
-	cin.getline(str1,30);
-	cout<<"Ingrese la segunda cadena: ";
-	cin.getline(str2,30);
-	num=str_cmp(str1,str2);
-	cout<<endl;
-	if(num==0){
-		cout<<str1<<" = "<<str2;
-	}else if(num==-1){
-		cout<<str1<<" < "<<str2;
-	}else {
-		cout<<str1<<" > "<<str2<<endl;
-	}
-	return 0;
-}#include<iostream>
-using namespace std;
-
-int str_cmp(char *str1,char *str2){
-	int cmp=0;
-	for(int i=0;i<30;i++){
-		
-		if(str1[i]<str2[i]){
-			cmp=-1;
+		}else{
+			cmp=0;
 			break;
-		}else if(str1[i]>str2[i]){
-			cmp=1;
-			break;
-		}
 	}
 	return cmp;
 }
